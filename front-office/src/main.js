909889cdd12020be37cee0fa778649c9764a6c32
joaopvs99 <jpvianasoares@gmail.com>
2022-05-08 21:59:53 +0100

    Update main.js

--- front-office/src/main.js
+++ front-office/src/main.js
@@ -3,6 +3,8 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
