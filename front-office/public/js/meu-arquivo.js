5820d00f0bc33af3902832841b5cd6959e324c88
DenerLucas <102250335+DenerLucas@users.noreply.github.com>
2022-05-30 23:36:31 +0100

    booooost FRONT-OFICCE

--- front-office/public/js/meu-arquivo.js
+++ front-office/public/js/meu-arquivo.js
@@ -3,7 +3,7 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('ImgPortfolio');
+var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
@@ -13,10 +13,26 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close1")[0];
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 } 
+
+// JavaScript code
+function BarraPesquisa() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('animals');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
