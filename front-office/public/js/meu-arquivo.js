056ab40b304af7b6018a6fb3272438cbfe513609
DenerLucas <102250335+DenerLucas@users.noreply.github.com>
2022-05-31 20:27:52 +0100

    smooth scrool javascrypt

--- front-office/public/js/meu-arquivo.js
+++ front-office/public/js/meu-arquivo.js
@@ -58,3 +58,14 @@ function guardarFunc() {
   
   
 } 
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
